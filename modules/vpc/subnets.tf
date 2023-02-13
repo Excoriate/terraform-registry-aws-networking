03ee195bfd0dbcc0b4e632b079372f164419f6a5
Alex ATorres <atorres.ruiz@hotmail.com>
2023-02-13 10:01:27 +0100

    feat: add tags to identify subnets

--- modules/vpc/subnets.tf
+++ modules/vpc/subnets.tf
@@ -5,6 +5,10 @@ resource "aws_subnet" "private" {
   cidr_block              = local.private_subnets_cidr_list[count.index]
   availability_zone       = local.availability_zones_names[count.index]
   map_public_ip_on_launch = var.subnet_config.map_public_ip_on_launch_subnet_private
+
+  tags = merge(var.tags, {
+    "Name" = "${var.tags["Name"]}-private-${local.availability_zones_names[count.index]}"
+  })
 }
 
 resource "aws_subnet" "public" {
@@ -14,4 +18,8 @@ resource "aws_subnet" "public" {
   cidr_block              = local.public_subnets_cidr_list[count.index]
   availability_zone       = local.availability_zones_names[count.index]
   map_public_ip_on_launch = var.subnet_config.map_public_ip_on_launch_subnet_public
+
+  tags = merge(var.tags, {
+    "Name" = "${var.tags["Name"]}-public-${local.availability_zones_names[count.index]}"
+  })
 }
