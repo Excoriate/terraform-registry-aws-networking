2461c4a56c33d6f8150c9a13460e6f16b36f13a8
Alex ATorres <atorres.ruiz@hotmail.com>
2023-03-14 05:40:57 +0100

    fix: fix lookup by zone in dns-records module

--- modules/route53-dns-records/locals.tf
+++ modules/route53-dns-records/locals.tf
@@ -16,29 +16,16 @@ locals {
       zone_id         = record["zone_id"] == null ? null : trimspace(record["zone_id"])
       allow_overwrite = record["allow_overwrite"] == null ? false : record["allow_overwrite"]
       alias = record["alias_target_config"] == null ? {} : {
-        name                   = record["alias_target_config"]["target_dns_name"] == null ? null : trimspace(record["alias_target_config"]["target_dns_name"])
-        zone_id                = record["alias_target_config"]["target_zone_id"] == null ? null : trimspace(record["alias_target_config"]["target_zone_id"])
+        name                   = trimspace(record["alias_target_config"]["target_dns_name"])
+        zone_id                = trimspace(record["alias_target_config"]["target_zone_id"])
         evaluate_target_health = record["alias_target_config"]["target_enable_health_check"] == null ? false : record["alias_target_config"]["target_enable_health_check"]
       }
       // Feature flags.
-      is_zone_lookup_by_zone_id_enabled   = record["zone_id"] == null
-      is_zone_lookup_by_zone_name_enabled = record["zone_name"] == null // Name will take precedence
+      is_zone_lookup_by_zone_id_enabled   = record["zone_id"] != null
+      is_zone_lookup_by_zone_name_enabled = record["zone_name"] != null // Name will take precedence
     }
   ]
 
   dns_alias_to_create = !local.is_dns_alias_enabled ? {} : {
-    for record in local.dns_alias_normalized : record["name"] => {
-      name                                = record["name"]
-      zone_name                           = record["zone_name"]
-      zone_id                             = record["zone_id"]
-      allow_overwrite                     = record["allow_overwrite"]
-      is_zone_lookup_by_zone_id_enabled   = record["is_zone_lookup_by_zone_id_enabled"]
-      is_zone_lookup_by_zone_name_enabled = record["is_zone_lookup_by_zone_name_enabled"]
-      alias = {
-        name                   = record["alias"]["name"]
-        zone_id                = record["alias"]["zone_id"]
-        evaluate_target_health = record["alias"]["evaluate_target_health"]
-      }
-    }
-  }
+  for record in local.dns_alias_normalized : record["name"] => record }
 }
